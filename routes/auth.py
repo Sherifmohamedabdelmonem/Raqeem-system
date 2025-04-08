5e72c81b321a8a650816eb5f31adc3369f7e1b97
Sherifmohamedabdelmonem <Sherif2996@gmail.com>
2025-04-09 01:35:03 +0200

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,1 +1,8 @@
-# auth.py logic here
+from flask import Blueprint
+
+auth = Blueprint('auth', _name_)
+
+# مثال لمسار تسجيل الدخول
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return "صفحة تسجيل الدخول"# auth.py logic here
