1a345a22a5efae08576c6a307fe11077e26a55ba
Sherifmohamedabdelmonem <Sherif2996@gmail.com>
2025-04-09 00:58:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ class Config:
     DATABASE = 'data.db'
     SMTP_SERVER = 'smtp.gmail.com'
     SMTP_PORT = 587
-    EMAIL_SENDER = 'your_email@gmail.com'
-    EMAIL_PASSWORD = 'your_email_app_password'
+    EMAIL_SENDER = 'raqeemsystem@gmail.com'
+    EMAIL_PASSWORD = 'raqeem01006797032%%%'
